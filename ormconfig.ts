90facc78e2f85536a090e86e1ad73e345a962ea5
NicholasWM <nicholaswm.42@gmail.com>
2022-10-23 09:12:29 -0300

    add: postgres datasource TypeORM

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,10 +1,30 @@
+import { AVAILABLE_DATA_SOURCES } from '@/@common/generics/generic.provider';
 import { DataSource } from 'typeorm';
 import { TypeOrmModels } from './src/@common/infra/db/typeorm/models';
 
 export const connectionSource = new DataSource({
+  name: AVAILABLE_DATA_SOURCES.TYPEORM.DEFAULT,
   type: 'mysql',
   host: 'localhost',
   port: 8801,
+  username: 'root',
+  password: 'study_projects',
+  database: 'backoffice_war_typeorm',
+  entities: TypeOrmModels,
+  synchronize: true,
+  // debug: true,
+  logging: ['query', 'error'],
+  migrations: ['./dist/@common/infra/db/typeorm/migrations/*.ts'],
+  // cli: {
+  //   migrationsDir: 'migration',
+  // }
+});
+
+export const connectionSourcePg = new DataSource({
+  name: AVAILABLE_DATA_SOURCES.TYPEORM.DB_2,
+  type: 'postgres',
+  host: 'localhost',
+  port: 8803,
   username: 'root',
   password: 'study_projects',
   database: 'backoffice_war_typeorm',
