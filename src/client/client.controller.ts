6918fd5dd1671f160677b8dd2ad4b9720d37f5fd
NicholasWM <nicholaswm.42@gmail.com>
2024-03-22 19:14:22 -0300

    add: api tags

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from '@nestjs/common';
 import { ClientService } from './client.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('# Client')
 @Controller('client')
 export class ClientController {
   constructor(private readonly clientService: ClientService) {}
