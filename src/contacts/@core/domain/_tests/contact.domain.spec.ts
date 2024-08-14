436eaf1b5e7f7cde52678f89709a953e9c898bc2
NicholasWM <nicholaswm.42@gmail.com>
2024-08-13 23:03:19 -0300

    fix: test

--- src/contacts/@core/domain/_tests/contact.domain.spec.ts
+++ src/contacts/@core/domain/_tests/contact.domain.spec.ts
@@ -1,13 +1,18 @@
+import { randomUUID } from 'crypto';
 import { Contact } from '../entity';
+import { ContactDTO } from '../entity/Contact.dto';
 
 describe('Contact Domain', () => {
   test('Must create a contact', () => {
-    const contactData: Pick<Contact, 'value' | 'type'> = {
+    const contactData: ContactDTO = {
       type: 'phone',
       value: '(13)12312-9923',
+      ownerId: randomUUID(),
+      status: '',
     };
 
-    const contact = new Contact(contactData.value, contactData.type);
+    // const contact = new Contact(contactData.value, contactData.type);
+    const contact = new Contact(contactData);
 
     expect(contact.value).toBe(contactData.value);
     expect(contact.type).toBe(contactData.type);
